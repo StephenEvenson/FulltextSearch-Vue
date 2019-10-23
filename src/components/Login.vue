3e932562a1f198240e7d1b684f2abba7a3028150
Erin <1580933790@qq.com>
2019-10-23 18:58:20 +0800

    first commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,10 +8,8 @@
     </el-form-item>
     <el-form-item prop="password">
       <el-input type="password" v-model="loginForm.password"
-                auto-complete="off" placeholder="密码"></el-input>
+                auto-complete="off" placeholder="密码" show-password></el-input>
     </el-form-item>
-    <el-checkbox class="login_remember" v-model="checked"
-                 label-position="left">记住密码</el-checkbox>
     <el-form-item style="width: 100%">
       <el-button type="primary" style="width: 100%" @click="submitClick">登录</el-button>
     </el-form-item>
@@ -27,8 +25,8 @@
         },
         checked: true,
         loginForm: {
-          username: 'admin',
-          password: '123'
+          username: '',
+          password: ''
         },
         loading: false
       }
@@ -38,16 +36,29 @@
         var _this = this;
         this.loading = true;
         this.postRequest('/login', {
-          username: this.loginForm.username,
+          user: this.loginForm.username,
           password: this.loginForm.password
         }).then(resp=> {
           _this.loading = false;
           if (resp && resp.status == 200) {
             var data = resp.data;
-            _this.$store.commit('login', data.obj);
+            if(!data.login){
+                if(data.at=="user"){
+                    _this.$message.error('用户名错误！');
+                }else if(data.at=="password"){
+                    _this.$message.error('密码错误！');
+                }
+            }else{
+                _this.$message({
+                    message: '登陆成功！',
+                    type: 'success'
+                });
+                _this.$router.replace("/home");
+            }
+           /* _this.$store.commit('login', data.obj);
             var path = _this.$route.query.redirect;
             _this.$router
-              .replace({path: path == '/' || path == undefined ? '/home' : path});
+              .replace({path: path == '/' || path == undefined ? '/home' : path});*/
           }
         });
       }
